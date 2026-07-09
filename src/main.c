6cfb2c267391b8f116c6cec6d991b860ed37bc2a
Abdelhak <bouzenadabdelhak@gmail.com>
2026-07-09 22:42:30 +0200

    Feat: Add main loop

---
+++ src/main.c
@@ -0,0 +1,50 @@
+#include "error.h"
+#include "instruction.h"
+#include "state.h"
+#include "fetch.h"
+#include "decode.h"
+#include "execute.h"
+#include "screen.h"
+#include "input.h"
+#include "parse.h"
+#include <ncurses.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  init_screen();
+  system_state_t state;
+  int p;
+  char* file_path = get_file_path(argc, argv);
+  if (file_path == NULL) {
+    printf("Missing or ambiguous file path");
+    return -1;
+  }
+  if ((p = init_state(&state, file_path)) < 0) {
+    printf("Error happened: %d\n", p);
+    return -1;
+  } 
+  while (state.registers.program_counter < MEMORY_SIZE) {
+    for (int i = 0; i < 8; i++) {
+      opcode_t opcode = fetch_next(&state);
+      instruction_t inst = decode(opcode);
+      int e;
+      if ((e = execute(&state, inst)) < 0) {
+        printf("%s\n", get_status_message(e));
+        printf("Program counter: %x\n", state.registers.program_counter);
+        printf("Last opcode executed: %x\n", opcode);
+        return e;
+      }
+    }
+    if (state.registers.delay_register > 0) {
+      state.registers.delay_register--;
+    }
+    if (state.registers.sound_register > 0) {
+      state.registers.sound_register--;
+    }
+    draw_screen(state);
+    monitor_input(&state);
+    usleep(16667);
+  }
+  printf("Reached end of memory");
+	return 0;
+}
