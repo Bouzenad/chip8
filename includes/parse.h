6db7863326432ce852c67b298986c6262f7a11c0
Abdelhak <bouzenadabdelhak@gmail.com>
2026-07-11 16:15:15 +0200

    Feat: Add sound frequency option

--- includes/parse.h
+++ includes/parse.h
@@ -1,9 +1,11 @@
 #ifndef PARSE_H
 #define PARSE_H
 
+#include "state.h"
 // Takes in arguments, and returns the filepath to a CHIP-8 ROM.
 // Anything that doesn't start with - counts as a candidate
 // Multiple paths added as arguments return NULL, due to ambiguity
 char* get_file_path(int argc, char *argv[]);
+int set_configs(system_state_t *state, int argc, char* argv[]);
 
 #endif
