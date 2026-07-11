2e42983cab6a4c8ae6085e685069cb7aafe25500
Abdelhak <bouzenadabdelhak@gmail.com>
2026-07-11 11:45:06 +0200

    Feat: Centered screen

--- src/screen.c
+++ src/screen.c
@@ -16,7 +16,7 @@ void draw_screen(system_state_t state) {
 	int x, y;
 	getmaxyx(stdscr, y, x);
 	for (int i = 0; i < DISPLAY_WIDTH * DISPLAY_HEIGHT; i++) {
-		char c = state.display[i / DISPLAY_WIDTH][i % DISPLAY_WIDTH] == 0 ? '.' : '#';
+		char c = state.display[i / DISPLAY_WIDTH][i % DISPLAY_WIDTH] == 0 ? ' ' : '#';
 		move(i / DISPLAY_WIDTH + (y - DISPLAY_HEIGHT) / 2, i % DISPLAY_WIDTH + (x - DISPLAY_WIDTH)/2);
 		printw("%c", c);
 	}
