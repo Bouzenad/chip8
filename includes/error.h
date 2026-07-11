a9f809430f66d19913c2e0d0da9f4e4913b9f7e7
Abdelhak <bouzenadabdelhak@gmail.com>
2026-07-11 16:14:15 +0200

    Doc: add documentation to error.h

--- includes/error.h
+++ includes/error.h
@@ -1,16 +1,18 @@
 #ifndef ERROR_H
 #define ERROR_H
 
+// Differnt kinds of error/status codes that can happen during execution
 typedef enum error_code {
-	STATUS_OK = 0,
-	GENERAL_ERROR = -1,
-	STACK_UNDERFLOW = -2,
-	STACK_OVERFLOW = -3,
-	MEMORY_OUT_OF_BOUNDS = -4,
-	UNKNOWN_INSTRUCTION = -5,
-	UNIMPLEMENTED_INSTRUCTION = -6,
+  STATUS_OK = 0,
+  GENERAL_ERROR = -1,
+  STACK_UNDERFLOW = -2,
+  STACK_OVERFLOW = -3,
+  MEMORY_OUT_OF_BOUNDS = -4,
+  UNKNOWN_INSTRUCTION = -5,
+  UNIMPLEMENTED_INSTRUCTION = -6,
 } error_code;
 
-char* get_status_message(error_code code);
+// Gives a human-readable message based on status
+char *get_status_message(error_code code);
 
 #endif
